6fc08bccaa4f336eb2a9b8d9777f7f1343f7687b
Yehudis Schnaidman <yehudis111@gmail.com>
2026-04-23 01:15:25 +0000

    tried implimanting better scraper

--- scraper.py
+++ scraper.py
@@ -48,4 +48,29 @@ def scrape_activities():
                 })
     return activities_data
 
+""" the code below did not work for scaping when I tried it"""
+# def scrape_visit_nepa():
+#     headers = {"User-Agent": "Mozilla/5.0"}
+#     base_url = "https://discovernepa.com/things-to-do/search/"
+#     page_num = 1
+#     page = requests.get(f"{base_url}?categories%5B%5D=52&categories%5B%5D=53&categories%5B%5D=61&categories%5B%5D=74&categories%5B%5D=93&categories%5B%5D=56&categories%5B%5D=97&categories%5B%5D=70&categories%5B%5D=72&categories%5B%5D=102&categories%5B%5D=99&categories%5B%5D=71&page_num={page_num}&location%5B%5D=269385&locationType%5B%5D=city", headers=headers)
+#     soup = BeautifulSoup(page.content, "html.parser")
+#     # dictionary with all the activities
+#     activities_data = []
+#     # html under landmarks and attractions
+#     items = soup.find_all("a", class_="vue-listing-card")
+#     # go through the heading
+#     for item in items:
+#         link = item.get("href", "")
+#         name = item.find("h3").text
+#         category = item.find("div", class_="eyebrow").text
+#         # add to the dictionary: only include activities ( not random <a>'s)
 
+#         activities_data.append({
+#             "name": name,
+#             "category":category,
+#             "link": link
+#             })
+#     return activities_data
+#https://www.visitnepa.org/plugins/maps/map/things-to-do-in-scranton/6503247a175f177b67c029f9/?embed=true
+        
